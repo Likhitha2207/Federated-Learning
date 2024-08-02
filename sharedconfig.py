e648105a408f0d06f7367d190ad79fd4e25c6970
Likhitha2207 <99482350+Likhitha2207@users.noreply.github.com>
2024-08-02 13:47:16 +0530

    Add files via upload

---
+++ sharedconfig.py
@@ -0,0 +1,15 @@
+username1="client1"
+password1="123@client"
+model_used = "LSTM"
+
+username2="client2"
+password2="321@client"
+model_used = "LSTM"
+
+username3="client3"
+password3="312@client"
+model_used = "LSTM"
+
+userver="server"
+pserver="788@server"
+model_used = "LSTM"
